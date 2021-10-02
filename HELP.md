935c0062209f0daf891692eafc0a27a0e90d98cd
att <summerokk@gmail.com>
2021-10-02 18:59:46 +0300

    Form login

--- HELP.md
+++ HELP.md
@@ -2,5 +2,6 @@
 
 * @PreAuthorize('') можно использовать вместо или вместе с SecurityConfig .antMatchers.hasRole. Нужна аннотация 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+* Чтобы переключиться на автризацию через html форму, нужно написать в конфиге .formLogin вместо .httpBasic
 
 
--- src/main/java/com/example/security/config/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/config/ApplicationSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.security.config;
 
+import ch.qos.logback.core.util.TimeUtil;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -41,7 +43,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/courses")
+                .and()
+                .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)).key("somesecuredkey")
+                .and()
+                .logout().logoutUrl("/logout")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me")
+                .logoutSuccessUrl("/login");
     }
 
     @Override
